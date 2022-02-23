eeefc31bb548b48ecf6ffc13d56cf95c761b1737
Chase Murtaugh <cmurtau@g.clemson.edu>
2022-02-23 12:46:44 -0500

    Codewars solutions

---
+++ codewars.js
@@ -0,0 +1,17 @@
+function printerError(s) {
+
+	const goodLetters = "abcdefghijklm"
+
+	let errors = 0
+
+	let totalLetters = s.length
+
+	for (const letters in s) {
+		if (!(letters in goodLetters)) {
+			const element = object[key];
+			
+		}
+	}
+
+	
+}
